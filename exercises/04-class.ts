1a20c995d4895bbb23c44137bafab06783c959e5
Yael <rayacova37@gmail.com>
2025-02-21 06:25:53 -0600

    add: interface Human for Person class

--- exercises/04-class.ts
+++ exercises/04-class.ts
@@ -1,24 +1,34 @@
-export class Human{
+interface Human{
+    name:string;
+    age: number,
+    birthdate: string;
+    sayAge():number;
+    saySomething(words: string): string;
+    sayMyName(): string;
+    sayNameAge(): string;
+}
+
+export class Person implements Human{ 
     name: string;
     age: number;
     birthdate: string;
     constructor(name: string, age:number,birthdate:string){
         this.name = name;
         this.age = age;
-        this.birthdate = birthdate
+        this.birthdate = birthdate;
     }
     sayAge(): number {
-        return this.age
+        return this.age;
     }
     saySomething(words: string){
-        return `${this.name}: ${words}`
+        return `${this.name}: ${words}`;
     }
-    sayMyName(){
-        return this.name
+    sayMyName(): string{
+        return this.name;
     }
-    sayNameAge(){
-        return `${this.name}: ${this.age}, ${new Date(this.birthdate).toLocaleDateString()}`
+    sayNameAge(): string{
+        return `${this.name}: ${this.age}, ${new Date(this.birthdate).toLocaleDateString()}`;
     }
 }
 
-export const Yael = new Human('Yael',21,'2003-11-05')
+export const Yael = new Person('Yael',21,'2003-11-05');

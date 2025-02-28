7a29c72b6277ad2cfa00517b87511c10d5c4a2a7
Yael <rayacova37@gmail.com>
2025-02-28 12:28:00 -0600

    refactor: change employee ID type to UUID and update related logic

--- first-pro/src/employees/employees.service.ts
+++ first-pro/src/employees/employees.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class EmployeesService {
   private employees: CreateEmployeeDto[] = [
     {
-      id: 1,
+      id: uuidv4(),
       name: 'Alberto',
       lastName: 'Gonzalez',
       phoneNumber: 'XXX123135',
     },
     {
-      id: 2,
+      id: uuidv4(),
       name: 'Maria',
       lastName: 'Gonzalez',
       phoneNumber: 'XXX123136',
     },
     {
-      id: 3,
+      id: uuidv4(),
       name: 'Juan',
       lastName: 'Gonzalez',
       phoneNumber: 'XXX123137',
@@ -26,7 +27,7 @@ export class EmployeesService {
   ];
 
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length;
+    createEmployeeDto.id = uuidv4();
     this.employees.push(createEmployeeDto);
     return createEmployeeDto;
   }
@@ -35,17 +36,19 @@ export class EmployeesService {
     return this.employees;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     const employee = this.employees.filter((employee) => employee.id === id)[0];
+    if (!employee) throw new NotFoundException();
     return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id);
     employeeToUpdate = {
       ...employeeToUpdate,
       ...updateEmployeeDto,
     };
+
     this.employees = this.employees.map((employee) => {
       if (employee.id === id) {
         employee = employeeToUpdate;
@@ -57,7 +60,8 @@ export class EmployeesService {
     return employeeToUpdate;
   }
 
-  remove(id: number) {
+  remove(id: string) {
+    this.findOne(id);
     this.employees = this.employees.filter((employees) => employees.id !== id);
     return this.employees;
   }

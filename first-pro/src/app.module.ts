9293d2a2adb979b1a7fdcca18f1c65b34c3d3574
Yael <rayacova37@gmail.com>
2025-03-02 23:49:06 -0600

    feat: add managers, locations, and regions modules with corresponding DTOs, entities, and services

--- first-pro/src/app.module.ts
+++ first-pro/src/app.module.ts
@@ -5,6 +5,9 @@ import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
@@ -22,6 +25,9 @@ import { ProvidersModule } from './providers/providers.module';
     EmployeesModule,
     ProductsModule,
     ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

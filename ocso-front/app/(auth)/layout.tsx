34f7b4945a4626189e0af8d563578d45ebc21e8d
Yael <rayacova37@gmail.com>
2025-03-22 14:05:53 -0600

    feat(frontend): update authentication pages with new layout and styling

--- ocso-front/app/(auth)/layout.tsx
+++ ocso-front/app/(auth)/layout.tsx
@@ -4,8 +4,10 @@ export default function AuthLayout({children}:
     Readonly<{children: React.ReactNode}>
 ){
     return(
-        <div className="bg-orange-200 w-screen h-screen overflow-hidden">
-            {children}
+        <div className="bg-cyan-50 w-screen h-screen overflow-hidden grid">
+            <div className="place-content-center place-self-center">
+                {children}
+            </div>
         </div>
     )
 }
--- ocso-front/app/(auth)/signup/page.tsx
+++ ocso-front/app/(auth)/signup/page.tsx
@@ -1,7 +1,47 @@
-import { Button } from "@heroui/button";
+import { Input } from "@heroui/react"
 
 export default function SignupPage(){
     return (
-        <Button color="primary">Button</Button>
+        <div className="flex items-center justify-center min-h-screen bg-cyan-50">
+        {/* Contenedor principal con fondo naranja */}
+        <div className="bg-cyan-700 p-6 rounded-lg w-full max-w-sm">
+          {/* Título */}
+            <h2 className="text-xl font-bold mb-4 text-white">
+                Registrarse en la plataforma
+            </h2>
+
+          {/* Campo de Email */}
+            <div className="mb-4 flex flex-col gap-4">
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md outline-none placeholder:text-amber-50/20 text-amber-50"
+                />
+
+
+
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md outline-none placeholder:text-amber-50/20 text-amber-50"
+                    />
+
+
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md outline-none placeholder:text-amber-50/20 text-amber-50"
+                    />
+            </div>
+                {/* Botón de Registrarse */}
+                <button className="bg-gray-300 text-neutral-800 py-1 px-4 rounded-lg cursor-pointer mt-4 place-self-center">
+                Registrarse
+                </button>
+
+                <p className="text-xs pt-4 justify-self-end">¿Ya tienes cuenta? <a href="/login" className="text-blue-100 underline">Iniciar Sesion</a></p>
+
+            </div>
+        </div>
     )
 }

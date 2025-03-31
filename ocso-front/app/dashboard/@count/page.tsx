fb38a0dc8d707f65e97bfe76837afeff7bdbc106
Yael <rayacova37@gmail.com>
2025-03-31 17:41:02 -0600

    feat(count): implement token-based authentication for count locations and add loading/error components

--- ocso-front/app/dashboard/@count/page.tsx
+++ ocso-front/app/dashboard/@count/page.tsx
@@ -1,7 +1,16 @@
+import { TOKEN_NAME } from "@/constants"
 import axios from "axios";
+import { cookies } from "next/headers"
 
 const CountPage = async () => {
-    const countLocations = await axios.get("http://172.28.224.1:3000/Locations")
+    const userCookies = cookies()
+    const token = (await userCookies).get(TOKEN_NAME)
+
+    const countLocations = await axios.get("http://172.28.224.1:3000/Locations", {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    })
 
     return countLocations?.data.length
 };

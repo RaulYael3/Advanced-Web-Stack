8e5abd5f386ce70f9c6e39a63a8b0b7d02a7811f
Yael <rayacova37@gmail.com>
2025-06-04 15:35:39 -0600

    feat(locations): add UpdateLocation component for managing location updates with modal interface

---
+++ ocso-front/app/dashboard/@locations/_components/UpdateLocation.tsx
@@ -0,0 +1,36 @@
+'use client'
+import {
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Button,
+	useDisclosure,
+} from '@heroui/react'
+
+export default function UpdateLocation({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+	return (
+		<>
+			<Button onPress={onOpen}>Open Modal</Button>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+				<ModalContent>
+					{() => (
+						<>
+							<ModalHeader className='flex flex-col gap-1'>
+								Modal Title
+							</ModalHeader>
+							<ModalBody>{children}</ModalBody>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
+	)
+}

4227c3631edf05f7d184f9aedaca7a2acbd87f92
Yael <rayacova37@gmail.com>
2025-06-04 17:39:52 -0600

    fix(layout): add path check for locations rendering in LayoutDashboard component

--- ocso-front/app/dashboard/layout.tsx
+++ ocso-front/app/dashboard/layout.tsx
@@ -1,21 +1,24 @@
-import Header from "./_components/Header"
-import Sidebar from "./_components/_sidebar/Sidebar"
+'use client'
+import { usePathname } from 'next/navigation'
+import Header from './_components/Header'
+import Sidebar from './_components/_sidebar/Sidebar'
 
 export default function LayoutDashboard({
-    children,
-    locations
-    }:Readonly<{
-        children: React.ReactNode
-        locations: React.ReactNode
-    }>){
-    return (
-        <div className="w-screen h-screen overflow-hidden grid bg-cyan-100">
-            <Header />
-            <div className="flex flex-row items-center">
-                <Sidebar />
-                {children}
-                {locations && <div>{locations}</div>}
-            </div>
-        </div>
-    )
+	children,
+	locations,
+}: Readonly<{
+	children: React.ReactNode
+	locations: React.ReactNode
+}>) {
+	const path = usePathname()
+	return (
+		<div className='w-screen h-screen overflow-hidden grid bg-cyan-100'>
+			<Header />
+			<div className='flex flex-row items-center'>
+				<Sidebar />
+				{children}
+				{locations && path === '/Dashboard' && <div>{locations}</div>}
+			</div>
+		</div>
+	)
 }

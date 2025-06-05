dd6688f04a7d62ec287b0167d56aab1e11c1a9cc
Yael <rayacova37@gmail.com>
2025-06-05 08:18:36 -0600

    feat(managers): implement create, delete, and update actions for manager management

---
+++ ocso-front/actions/managers/delete.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { API_URL } from "@/constants"
+import { authHeaders } from "@/helpers/authHeaders"
+import { revalidateTag } from "next/cache"
+
+export default async function createManager(managerId: string, formData: FormData){
+    let manager: any = {}
+    for(const key of formData.keys()) {
+        manager[key] = formData.get(key)
+    }
+
+    const headers = await authHeaders()
+    const response  = await fetch(`${API_URL}/managers${managerId}`, {
+        method: 'DELETE',
+        headers: {
+            ...headers,
+        }
+    })
+    revalidateTag("dashboard:managers")
+}
---
+++ ocso-front/actions/managers/update.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import { API_URL } from "@/constants"
+import { authHeaders } from "@/helpers/authHeaders"
+import { revalidateTag } from "next/cache"
+
+export default async function createManager(managerId: string, formData: FormData){
+    let manager: any = {}
+    for(const key of formData.keys()) {
+        manager[key] = formData.get(key)
+    }
+
+    const headers = await authHeaders()
+    const response  = await fetch(`${API_URL}/managers`, {
+        method: 'PATCH',
+        body: JSON.stringify(manager),
+        headers: {
+            ...headers,
+        }
+    })
+    if(response.status === 201){
+        revalidateTag("dashboard:managers")
+        revalidateTag(`dashboard:managers${managerId}`)
+    }
+}

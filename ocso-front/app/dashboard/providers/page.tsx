5c017b36f6beb254ec283475eeec99bb2c9a5318
Yael <rayacova37@gmail.com>
2025-06-05 11:45:48 -0600

    feat(providers): update provider page layout with button for adding new providers and improved styling

--- ocso-front/app/dashboard/providers/page.tsx
+++ ocso-front/app/dashboard/providers/page.tsx
@@ -3,6 +3,8 @@ import { authHeaders } from '@/helpers/authHeaders'
 import ProviderCard from './_components/ProviderCard'
 import { Provider } from '@/entities'
 import Link from 'next/link'
+import { Button } from '@heroui/button'
+import { LuPlus } from 'react-icons/lu'
 
 export default async function ProviderPage() {
 	const response = await fetch(`${API_URL}/providers`, {
@@ -13,19 +15,24 @@ export default async function ProviderPage() {
 	const providers: Provider[] = await response.json()
 
 	return (
-		<div className='flex w-full flex-row flex-grow-0 flex-wrap px-10'>
-			{providers.map((provider) => (
-				<Link
-					href={`/dashboard/providers/${provider.providerId}`}
-					className='w-4/12'
-					key={provider.providerId}
-				>
-					<ProviderCard
+		<div className='flex flex-col h-[90vh] w-full items-end px-10'>
+			<Button className='w-fit' color='primary'>
+				<LuPlus size={20} />
+			</Button>
+			<div className='flex w-full flex-grow-0 flex-wrap gap-20'>
+				{providers.map((provider) => (
+					<Link
+						href={`/dashboard/providers/${provider.providerId}`}
+						className='w-4/12 hover:scale-105 transition-transform duration-300'
 						key={provider.providerId}
-						provider={provider}
-					/>
-				</Link>
-			))}
+					>
+						<ProviderCard
+							key={provider.providerId}
+							provider={provider}
+						/>
+					</Link>
+				))}
+			</div>
 		</div>
 	)
 }

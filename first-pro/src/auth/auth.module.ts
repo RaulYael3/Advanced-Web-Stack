42abe8ee5c4bd178e4b2819dfefebd832ec15ca8
Yael <rayacova37@gmail.com>
2025-06-10 15:44:18 -0600

    feat(auth): include Employee and Manager entities in TypeOrmModule imports

--- first-pro/src/auth/auth.module.ts
+++ first-pro/src/auth/auth.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { JwtModule } from '@nestjs/jwt'
+import { Employee } from 'src/employees/entities/employee.entity'
+import { Manager } from 'src/managers/entities/manager.entity'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Employee, Manager]),
     JwtModule.register({
       secret: 'SECRET KEY',
       signOptions: {

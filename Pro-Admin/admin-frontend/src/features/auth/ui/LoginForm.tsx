499661bba7f273f2d4343d627dacafa3af76d247
Yael <rayacova37@gmail.com>
2025-06-12 20:43:03 -0600

    feat(auth, ui): update authentication page layout and styles; enhance LoginForm component with improved UI and error handling

--- Pro-Admin/admin-frontend/src/features/auth/ui/LoginForm.tsx
+++ Pro-Admin/admin-frontend/src/features/auth/ui/LoginForm.tsx
@@ -4,59 +4,93 @@ import { useAuthStore } from '../model/store'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 
 export const LoginForm = () => {
-  const { loginForm, setLoginForm, login } = useAuthStore()
+	const { loginForm, setLoginForm, login } = useAuthStore()
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    login()
-  }
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+		login()
+	}
 
-  return (
-    <Card className='border-none shadow-none bg-[#fbf7f6]' style={{boxShadow: 'inset 6px 6px 20px rgb(255, 255, 255), 6px 6px 20px -15px rgba(36, 26, 0, 0.47)'}}>
-      <CardHeader>
-        <CardTitle>Iniciar Sesión</CardTitle>
-        <CardDescription>Ingresa tus credenciales para acceder al panel</CardDescription>
-      </CardHeader>
-      <form onSubmit={handleSubmit}>
-        <CardContent className="space-y-4 gap-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="tu@email.com"
-              value={loginForm.email}
-              onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
-              required
-              className='border-none bg-transparent'
-              style={{boxShadow:'inset 6px 6px 20px -15px rgba(36, 26, 0, 0.47)'}}
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Contraseña</Label>
-            <Input
-              id="password"
-              type="password"
-              value={loginForm.password}
-              onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
-              required
-              className='border-none bg-transparent'
-              style={{boxShadow:'inset 6px 6px 20px -15px rgba(36, 26, 0, 0.47)'}}
-            />
-          </div>
-        </CardContent>
-        <CardFooter className='mt-8'>
-          <Button type="submit" 
-                  className="w-full cursor-pointer border-none bg-transparent text-black"
-                  style={{boxShadow: 'inset 6px 6px 20px rgb(255, 255, 255), 6px 6px 20px -15px rgba(36, 26, 0, 0.47)'}}
-          >
-            Iniciar Sesión
-          </Button>
-        </CardFooter>
-      </form>
-    </Card>
-  )
-} 
+	return (
+		<Card
+			className='border-none shadow-none bg-transparent'
+			style={{
+				boxShadow: '-6px -6px 20px 10px #FBF7F6, 6px 6px 20px #9b9c98',
+			}}
+		>
+			<CardHeader>
+				<CardTitle>Iniciar Sesión</CardTitle>
+				<CardDescription>
+					Ingresa tus credenciales para acceder al panel
+				</CardDescription>
+			</CardHeader>
+			<form onSubmit={handleSubmit}>
+				<CardContent className='space-y-4 gap-4'>
+					<div className='space-y-2'>
+						<Label htmlFor='email'>Email</Label>
+						<Input
+							id='email'
+							type='email'
+							placeholder='tu@email.com'
+							value={loginForm.email}
+							onChange={(e) =>
+								setLoginForm({
+									...loginForm,
+									email: e.target.value,
+								})
+							}
+							required
+							className='border-none bg-transparent'
+							style={{
+								boxShadow:
+									'inset 6px 6px 20px -15px rgba(36, 26, 0, 0.47), inset -6px -6px 20px  #FBF7F6',
+							}}
+						/>
+					</div>
+					<div className='space-y-2'>
+						<Label htmlFor='password'>Contraseña</Label>
+						<Input
+							id='password'
+							type='password'
+							value={loginForm.password}
+							onChange={(e) =>
+								setLoginForm({
+									...loginForm,
+									password: e.target.value,
+								})
+							}
+							required
+							className='border-none bg-transparent'
+							style={{
+								boxShadow:
+									'inset 6px 6px 20px -15px rgba(36, 26, 0, 0.47), inset -6px -6px 20px  #FBF7F6',
+							}}
+						/>
+					</div>
+				</CardContent>
+				<CardFooter className='mt-8'>
+					<Button
+						type='submit'
+						className='w-full cursor-pointer border-none bg-transparent text-gray-900'
+						style={{
+							boxShadow:
+								'-6px -6px 20px 10px #FBF7F6, 6px 6px 20px #9b9c98',
+						}}
+					>
+						Iniciar Sesión
+					</Button>
+				</CardFooter>
+			</form>
+		</Card>
+	)
+}

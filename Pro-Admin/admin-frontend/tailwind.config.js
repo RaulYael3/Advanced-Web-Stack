e1d53b2cb16feaa9590db8d73526fba11452dd4a
Yael <rayacova37@gmail.com>
2025-06-12 21:17:43 -0600

    feat(styles): update color variables in globals.css for improved theming; remove deprecated brand colors from tailwind.config.js

--- Pro-Admin/admin-frontend/tailwind.config.js
+++ Pro-Admin/admin-frontend/tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
-		extend: {
-			colors: {
-				brand: {
-					900: '#7b7d79',
-					800: '#9b9c98',
-					700: '#bbbbb6',
-					600: '#dad9d5',
-					500: '#f9f7f3',
-					400: '#faf7f5',
-					300: '#fbf7f6',
-					200: '#fcf8f8',
-					100: '#fdf9fa',
-				},
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 }
